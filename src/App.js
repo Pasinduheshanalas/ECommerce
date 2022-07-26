fdfff669881a4ffddde2a5f0d89e464044aae357
Pasinduheshanalas <pasindualas96@gmail.com>
2022-07-26 09:44:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import './App.css'
-import Grid from './component/grid';
+import './App.css';
+import Homepage from './component/homepage.component';
+
 
 function App() {
-
-    return(
-     <>
-     <Grid name ="heshan">Children</Grid>
-     </>
-
-    )
-  
-
+ return(
+<div>
+  <Homepage/>
+</div>
+ )
 }
 export default App;
